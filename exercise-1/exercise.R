2fc311f834576f23abbe507d93b4495f9af9b2a2
Joel Ross <joelross@uw.edu>
2016-12-23 16:35:42 -0800

    Update exercises for students

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,21 +2,15 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-AddThree <- function(value) {
-  return(value + 3)
-}
+
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-ten <- AddThree(7)
+
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-ImperialToMetric <- function(feet, inches) {
-  total.inches <- feet*12 + inches
-  return(total.inches * 0.0254)
-}
+
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
-height.in.meters <- ImperialToMetric(5,11)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,27 +3,14 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-CompareLength <- function(str1, str2) {
-  diff <- abs(nchar(str1) - nchar(str2))
-  diff.string <- paste('The difference in length is', diff)
-  return(diff.string)
-}
+
 
 # Pass two strings of different lengths to your `CompareLength` function
-CompareLength("Hey", "Hello ladies and gentlemen, how are you today?")
+
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-DescribeDifference <- function(a,b) {
-  diff <- nchar(a) - nchar(b)
-  if(diff > 0) {
-    sentence <- paste('Your first string is longer by', diff, 'characters')
-  } else {
-    sentence <- paste('Your second string is longer by', -diff, 'characters')
-  }
-  return(sentence)
-}
+
 
 # Pass two strings of different lengths to your `DescribeDifference` function
-DescribeDifference("Abracadabra!", "Presto!")
