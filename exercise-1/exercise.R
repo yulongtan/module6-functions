76a5ebbd928cdd9ba81d8efc7f02a10a4269b891
Yulong Tan <yulongtan67@gmail.com>
2017-01-12 13:34:54 -0800

    finish exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,20 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(num) {
+  return(num + 3)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(feet, inches) {
+  return(0.3048 * feet + 0.0254 * inches)
+}
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters <- ImperialToMetric(5, 8)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,14 +3,29 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(s1, s2) {
+  length1 <- nchar(s1)
+  length2 <- nchar(s2)
+  difference <- abs(length1 - length2)
+  return(paste('The difference in length is', difference, sep=' '))
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+print(CompareLength('hello', 'world'))
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(s1, s2) {
+  length1 <- nchar(s1)
+  length2 <- nchar(s2)
+  difference <- abs(length1 - length2)
+  if (length1 > length2) {
+    return(paste('Your first string is longer by', difference, 'characters', sep=' '))
+  } else {
+    return(paste('Your second string is longer by', difference, 'characters', sep=' '))
+  }
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+print(DescribeDifference('Hello', 'Terminal'))
